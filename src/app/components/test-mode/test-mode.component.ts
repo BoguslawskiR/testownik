2bef0667a49faa2e21a01d0fd2b798bf6ac14254
Radosław Bogusławski <bogusjr26@gmail.com>
2017-03-05 14:17:32 +0100

    Test check

--- src/app/components/test-mode/test-mode.component.ts
+++ src/app/components/test-mode/test-mode.component.ts
@@ -13,6 +13,7 @@ export class TestComponent implements OnInit {
     private test
     private question;
     private checkVal;
+    private result = []
 
     constructor(
       private _ts: TestsService,
@@ -24,21 +25,40 @@ export class TestComponent implements OnInit {
     ngOnInit(){
         this._route.params.switchMap(p=> this._ts.getTest(p["testId"])).subscribe(t => {
             this.test = t;
-            console.log(this.test);
+
             this.getQuestion();
         })
     }
 
      getQuestion(){
-        console.log("fnc")
+
         let rand = Math.floor((Math.random() * this.test.questions.length));
         this.question = this.test.questions[rand];
-        console.log(this.question)
+        
         this.checkVal=false;
+
+        this.result = []
     }
 
     check(){
         this.checkVal=true;
+        console.log(this.question.answers)
+        this.question.answers.forEach(x=>{
+            if(x.checked == true && x.is_correct == false) this.result.push('e');
+            if(x.checked != true && x.is_correct == true) this.result.push('e');
+        })
+        console.log(this.result)
+
+
+    }
+
+    changeVal(i){
+        if(this.question.answers[i].checked == true){
+            this.question.answers[i].checked = false;
+        } else {
+            this.question.answers[i].checked = true;
+        }
+
     }
 
 
